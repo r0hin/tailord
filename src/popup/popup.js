f3f61a0a717f0ae1637cb75a1423c75b8480e493
Rohin <git@r0h.in>
2023-08-14 23:37:22 -0400

    Everything
    - Users creating an account will preserve scan data created as a guest
    - Measurements tab (conditionally shown)
    - Redesign scan tab
    - Redesign account tab
    - Option to delete account
    - Favicon things

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,5 +1,5 @@
 import { firebaseApp } from './firebase_config'
-import { getAuth, onAuthStateChanged, setPersistence, browserLocalPersistence, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, signInAnonymously } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, setPersistence, browserLocalPersistence, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, signInAnonymously, EmailAuthProvider, reauthenticateWithCredential } from 'firebase/auth';
 import { doc, getDoc, getFirestore, onSnapshot, setDoc, updateDoc } from "firebase/firestore"
 import QRCode from "qrcode"
 
@@ -19,6 +19,7 @@ onAuthStateChanged(auth, user => {
       console.log('Settings saved');
     });
 
+    $(`#email`).text(user.email);
   }
   else {
     $(`#signedOut`).removeClass("hidden");
@@ -26,6 +27,7 @@ onAuthStateChanged(auth, user => {
 
     // Basically sign in anonymously
     signInAnonymously(auth)
+    $(`#email`).text("Guest")
   }
 
   if (user && user.uid) {
@@ -42,19 +44,59 @@ onAuthStateChanged(auth, user => {
 });
 
 async function loadMeasurements(user) {
-  await onSnapshot(doc(db, `users/${user.uid}`), (userDoc) => {
+  onSnapshot(doc(db, `users/${user.uid}`), async (userDoc) => {
     cacheUser = userDoc.data();
+
+    // Import sample data
+    // const jacksdock = await getDoc(doc(db, `users/4bNJ7Z6WzKTqvpRjaBz4IpolIZ13`));
+    // const jacksdockMeasurements = jacksdock.data().measurements;
+    // updateDoc(doc(db, `users/${user.uid}`), {
+    //   measurements: jacksdockMeasurements,
+    // });
+
     if (userDoc.exists() && Object.keys(userDoc.data().measurements || {}).length) {
       // Measurements exist
       $(`#noMeasurements`).addClass("hidden");
       $(`#yesMeasurements`).removeClass("hidden");
       $(`#beginScanButton`).html(`Scan Again`);
+
+      if (Object.keys(userDoc.data().measurements).length) {
+        $(`#measurementsTabButton`).removeClass("hidden");
+      }
+
+      // Populate measurements
+      $(`#age`).text(userDoc.data().measurements.age);
+      $(`#height`).text(userDoc.data().measurements.height);
+      $(`#weight`).text(userDoc.data().measurements.weight);
+      $(`#gender`).text(userDoc.data().measurements.gender);
+
+      $(`#measurementTable`).empty();
+      $(`#measurementTable`).append(`
+        <tr>
+          <th>Measurement</th>
+          <th>Value (In)</th>
+        </tr>
+      `)
+      userDoc.data().measurements.measurement.forEach((measurement) => {
+        $(`#measurementTable`).append(`
+          <tr>
+            <td class="measurementTableNameCell"><i id="infoButton${measurement.pointName}" class="bx bx-info-circle"></i> ${measurement.displayName}</td>
+            <td>${measurement.valueIninch}</td>
+          </tr>
+        `)
+
+        $(`#infoButton${measurement.pointName}`).get(0).onclick = () => {
+          alert(measurement.description);
+        }
+      })
+
     }
     else {
       // No measurements exist
       $(`#noMeasurements`).removeClass("hidden");
       $(`#yesMeasurements`).addClass("hidden");
-      $(`#beginScanButton`).html(`Begin Scan`);
+      $(`#beginScanButton`).html(`Create a Scan`);
+      $(`#measurementsTabButton`).addClass("hidden");
     }
 
     if (userDoc.exists() && userDoc.data().latest_access_code) {
@@ -126,21 +168,46 @@ $(`#endScanButton`).get(0).onclick = async () => {
   })
 
   const jsonData = await response.json();
-
   const data = jsonData.data;
-
+  console.log(data)
+  if (!data) {
+    alert("No measurements imported.")
+    updateDoc(doc(db, `users/${user.uid}`), {
+      latest_access_code: "",
+    });
+    return;
+  }
   updateDoc(doc(db, `users/${user.uid}`), {
     latest_access_code: "",
     measurements: data,
   });
+
 }
 
 $(`#signInButton`).get(0).onclick = async () => {
   const email = $(`#emailBox`).val();
   const password = $(`#passwordBox`).val();
+  let toPreserveMeasurements = false;
+  const measurements = cacheUser ? cacheUser.measurements : null;
 
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
+  // If already have measurements
+  if (cacheUser && Object.keys(cacheUser.measurements || {}).length) {
+    toPreserveMeasurements = true;
+  }
+
+  console.log(toPreserveMeasurements, measurements)
+  try {    
+    const credential = await signInWithEmailAndPassword(auth, email, password);
+
+    const existingUserDoc = await getDoc(doc(db, `users/${credential.user.uid}`));
+    if (toPreserveMeasurements && (measurements !== null)) {
+      if (!existingUserDoc.exists() || !existingUserDoc.data().measurements) {
+        // No measurements exist
+        await setDoc(doc(db, `users/${credential.user.uid}`), {
+          measurements: measurements,
+        });
+      }
+    }
   } catch (error) {
     alert(error.message);
   }
@@ -150,8 +217,26 @@ $(`#signUpButton`).get(0).onclick = async () => {
   const email = $(`#emailBox`).val();
   const password = $(`#passwordBox`).val();
 
+  let toPreserveMeasurements = false;
+  const measurements = cacheUser ? cacheUser.measurements : null;
+
+  // If already have measurements
+  if (cacheUser && Object.keys(cacheUser.measurements || {}).length) {
+    toPreserveMeasurements = true;
+  }
+
   try {
-    await createUserWithEmailAndPassword(auth, email, password);
+    const credential = await createUserWithEmailAndPassword(auth, email, password);
+
+    const existingUserDoc = await getDoc(doc(db, `users/${credential.user.uid}`));
+    if (toPreserveMeasurements && (measurements !== null)) {
+      if (!existingUserDoc.exists() || !existingUserDoc.data().measurements) {
+        // No measurements exist
+        await setDoc(doc(db, `users/${credential.user.uid}`), {
+          measurements: measurements,
+        });
+      }
+    }
   } catch (error) {
     alert(error.message);
   }
@@ -175,4 +260,40 @@ $(`#scanTabButton`).get(0).onclick = () => {
 
 $(`#settingsTabButton`).get(0).onclick = () => {
   switchTab("settings");
+}
+
+$(`#measurementsTabButton`).get(0).onclick = () => {
+  switchTab("measurements");
+}
+
+$(`#deleteAccountButton`).get(0).onclick = () => {
+  // Confirmation
+  const confirmed = confirm("Are you sure you want to delete your account? This action cannot be undone.");
+  if (!confirmed) {
+    return;
+  }
+
+  // Prompt for password
+  const password = prompt("Please enter your password to confirm deletion.");
+  if (!password) {
+    return;
+  }
+
+  // Reauthenticate
+  const credential = EmailAuthProvider.credential(
+    user.email,
+    password
+  );
+
+  reauthenticateWithCredential(user, credential).then((credential) => {
+    // User re-authenticated.
+    credential.user.delete().then(() => {
+      // User deleted.
+      alert("Account deleted successfully.")
+    })
+  })
+}
+
+$(`#contactSupport`).get(0).onclick = () => {
+  window.open("mailto:support@tailord.ca");
 }
